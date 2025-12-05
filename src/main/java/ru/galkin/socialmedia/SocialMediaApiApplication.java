b7f336867e142c5b39a3a8e19cef6fea43a701d0
galkinki <galkin@safe-line.ru>
2025-12-05 13:22:49 +0300

    first commit

---
+++ src/main/java/ru/galkin/socialmedia/SocialMediaApiApplication.java
@@ -0,0 +1,13 @@
+package ru.galkin.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/galkin/socialmedia/SocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.galkin.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
