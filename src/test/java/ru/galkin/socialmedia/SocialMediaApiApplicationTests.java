bd157b35b4430ce8b160ce30af423308d3cfae94
galkinki <galkin@safe-line.ru>
2025-12-06 21:30:21 +0300

    feature: Доменная модель [#505091]
    - Спроектировать доменную модель, создать changesets для Liquibase;
    - Настроить загрузку Liquibase через Spring boot

--- src/test/java/ru/galkin/socialmedia/SocialMediaApiApplicationTests.java
+++ src/test/java/ru/galkin/socialmedia/SocialMediaApiApplicationTests.java
@@ -2,8 +2,10 @@ package ru.galkin.socialmedia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SocialMediaApiApplicationTests {
 
 	@Test
